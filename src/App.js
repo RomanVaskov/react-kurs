3a883fc91b3969722f7a60e23780374cb61001f9
Roman <romavaskov@yandex.ru>
2020-02-13 14:20:02 +0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World!!!</p>
+    <div>
+      <Header />
+      <Tehnologies />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">New Feeds</a>
+      <a href="#s">Messages</a>
+    </div>
+  );
+}
+
+const Tehnologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>REACT</li>
+      </ul>
     </div>
   );
 }
