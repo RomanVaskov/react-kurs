dabcc2e6e75eafd7d2935873159f74eea313d451
Roman <romavaskov@yandex.ru>
2020-02-14 10:55:25 +0300

    Незначительные изменения

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Header from './Header';
-import Tehnologies from './Tehnologies';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Tehnologies />
     </div>
   );
 }
