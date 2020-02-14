e010cc083093e9992d72e8e61a72c7b95c8e2f58
Roman <romavaskov@yandex.ru>
2020-02-14 12:50:54 +0300

    Разбил будущее приложение на компоненты

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://static.vecteezy.com/system/resources/previews/000/039/568/original/hawx-logo-vector.jpg" alt="" />
+        </header>
+    )
+}
+
+export default Header;
