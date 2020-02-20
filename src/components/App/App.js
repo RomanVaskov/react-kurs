42770b02a81e35d7b9e4faa561b3c9d3ebe80121
Roman <romavaskov@yandex.ru>
2020-02-20 13:39:15 +0300

    Добавил новую компоненту Dialogs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import style from './App.module.css';
 import Header from '../Header';
 import Navbar from '../Navbar';
-import Profile from '../Profile';
+import Dialogs from '../Dialogs';
+// import Profile from '../Profile';
+
 
 function App() {
   return (
     <div className={style.app_wrapper}>
       <Header />
       <Navbar />
-      <Profile />
+      <div className={style.app_wrapper_content}>
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div>
   );
 }
