2ae3928f8735bf95fa697b08ba6ee9adfd5eb4b5
Roman <romavaskov@yandex.ru>
2020-02-25 15:00:28 +0300

    Правки в компоненте Dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import style from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <div className={style.dialog + ' ' + style.active}>Роман</div>
-                <div className={style.dialog}>Алексей</div>
-                <div className={style.dialog}>Дмитрий</div>
-                <div className={style.dialog}>Роман</div>
+                <div className={style.dialog + ' ' + style.active}>
+                    <NavLink to="/dialogs/1">Роман</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/2">Алексей</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/3">Дмитрий</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/4">Роман</NavLink>
+                </div>
             </div>
             <div className={style.messages}>
                 <div className={style.message}>PHP</div>
