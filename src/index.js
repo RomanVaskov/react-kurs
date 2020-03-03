3e3b0ddfedd72f9ad0439cb565230994a795b98a
Roman <romavaskov@yandex.ru>
2020-03-03 16:42:49 +0300

    Незначительные изменения в базе

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import { rerenderEntireTree } from './render'
-import state from './services/db'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import state, { subscribe, addPost, updateNewPostText } from './services/db';
+import './index.css';
+import App from './components/App/App';
 
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />, document.getElementById('root'));
+}
 rerenderEntireTree(state);
+subscribe(rerenderEntireTree);
--- src/render.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import { addPost, updateNewPostText } from "./services/db";
-
-export const rerenderEntireTree = (state) => {
-  ReactDOM.render(<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />, document.getElementById('root'));
-}
