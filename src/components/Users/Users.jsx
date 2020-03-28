dedcccc0f64d170d5bd09871d01024f9465932be
Roman <romavaskov@yandex.ru>
2020-03-28 21:23:34 +0300

    Работа с компонентой Users, сделал постраничный вывод пользователей

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,15 +6,54 @@ import usersPhoto from "../../assets/img/user.png";
 class Users extends Component {
   componentDidMount() {
     axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
       .then(response => {
         this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount / 100);
       });
   }
 
+  onPageChange = pageNumber => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then(response => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
   render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
     return (
       <div>
+        <div>
+          {pages.map(item => {
+            return (
+              <span
+                className={
+                  this.props.currentPage === item && style.selectedPage
+                }
+                onClick={() => {
+                  this.onPageChange(item);
+                }}
+              >
+                {item}
+              </span>
+            );
+          })}
+        </div>
         {this.props.users.map(u => {
           return (
             <div className={style.wrapper} key={u.id}>
