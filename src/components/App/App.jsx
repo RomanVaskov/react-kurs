c3561e3a6c668955c8ae845ac4e9960663099443
Roman <romavaskov@yandex.ru>
2020-05-16 12:44:57 +0300

    Настройка приложения для выгрузки на github pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -82,7 +82,7 @@ let AppContainer = compose(
 
 let ReactJsApp = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
